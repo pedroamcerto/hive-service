7e42b755ac37a8f595473fbc9e37bc148c68cd7b
Pedro Certo <113721862+pedroamcerto@users.noreply.github.com>
2025-06-05 02:04:03 -0300

    create: readme

--- src/main/java/com/hive/utils/mission/MissionUtils.java
+++ src/main/java/com/hive/utils/mission/MissionUtils.java
@@ -1,6 +1,7 @@
 package com.hive.utils.mission;
 
 import com.hive.domain.CarbonCredit;
+import com.hive.dto.carbonCredit.GetCarbonCredit;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,11 +13,11 @@ import java.util.List;
  */
 @Component
 public class MissionUtils {
-
     /**
      * Calcula o total de toneladas de carbono associadas a uma lista de créditos de carbono.
+     * Este método é usado quando a lista contém objetos do tipo `CarbonCredit`.
      *
-     * @param carbonCredits Lista de créditos de carbono.
+     * @param carbonCredits Lista de créditos de carbono do tipo `CarbonCredit`.
      * @return O total de toneladas de carbono somado a partir dos créditos de carbono fornecidos.
      */
     public Double calculateTotalTon(List<CarbonCredit> carbonCredits) {
@@ -29,4 +30,24 @@ public class MissionUtils {
 
         return totalTon;
     }
+
+    /**
+     * Calcula o total de toneladas de carbono associadas a uma lista de numeros de créditos de carbono.
+     * Este método é usado quando a lista contém objetos do tipo `Double`.
+     *
+     * @param carbonCredits Lista de valores de créditos de carbono do tipo `Double`.
+     * @return O total de toneladas de carbono somado a partir dos créditos de carbono fornecidos.
+     */
+    public Double calculateTotalTon(Double[] carbonCredits) {
+        Double totalTon = 0.0;
+
+        // Itera sobre cada crédito de carbono e soma o valor de carbono
+        for (Double carbonCredit : carbonCredits) {
+            totalTon += carbonCredit;
+        }
+
+        return totalTon;
+    }
+
+
 }
