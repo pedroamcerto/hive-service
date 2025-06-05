28c997aca2b64d7c1bd1e6c810cb3c353deebbd2
Pedro Certo <113721862+pedroamcerto@users.noreply.github.com>
2025-06-04 21:57:42 -0300

    create: carbon credit service

--- src/main/java/com/hive/dto/carbonCredit/GetCarbonCredit.java
+++ src/main/java/com/hive/dto/carbonCredit/GetCarbonCredit.java
@@ -1,12 +1,14 @@
 package com.hive.dto.carbonCredit;
 
+import com.hive.dto.mission.GetMissionDto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-public class GetCarbonCreditMissionDto {
+public class GetCarbonCredit {
     private Long id;
     private LocalDateTime emmitDate;
     private Double carbonTon;
+    private GetMissionDto mission;
 }
